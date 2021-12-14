be0097e2e89781a341a1cae828f0d13bf31b20d1
ata <ataturkoglu@outlook.com.tr>
2021-12-15 00:51:16 +0300

    :sparkles: feat: add router

---
+++ client/src/router/router.js
@@ -0,0 +1,27 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
+import Dashboard from '../views/Dashboard.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: Dashboard,
+  },
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
